be010a0c692116d8f213fb84eb082592982aad57
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-08-29 12:36:18 -0400

    init: create basic demo

---
+++ README.md
@@ -0,0 +1,13 @@
+# rust-cli-demo
+![GitHub License](https://img.shields.io/github/license/sebastian-j-ibanez/rust-cli-demo?color=orange)
+
+This repo is a demo on how to handle raw terminal input in Rust with 0 dependencies (via libc).
+
+The goal is to figure out how to get a polished REPL experience like in BASH, ZSH, or PowerShell.
+
+### Status
+
+The demo supports:
+ - [x] Moving the cursor left/right.
+ - [ ] Cycling between old commands with up/down.
+ - [ ] Multi-line commands.
---
+++ src/main.rs
@@ -0,0 +1,175 @@
+use scopeguard::guard;
+use std::{
+    io::{self, Read, Write},
+    os::fd::AsRawFd,
+    str,
+};
+
+enum InputState {
+    Normal,
+    Escape,
+    BracketedEscape,
+}
+
+fn main() -> io::Result<()> {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let fd = stdin.as_raw_fd();
+
+    let orig_termios = unsafe {
+        let mut orig_termios: libc::termios = std::mem::zeroed();
+        if libc::tcgetattr(fd, &mut orig_termios) != 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+        let mut raw = orig_termios;
+        raw.c_lflag &= !(libc::ICANON | libc::ECHO);
+        raw.c_cc[libc::VMIN] = 1;
+        raw.c_cc[libc::VTIME] = 0;
+        libc::tcsetattr(fd, libc::TCSANOW, &raw);
+        orig_termios
+    };
+
+    let _restore_termios = guard(orig_termios, |orig_termios| unsafe {
+        libc::tcsetattr(fd, libc::TCSANOW, &orig_termios);
+    });
+
+    let mut line = String::new();
+    let mut cursor_pos = 0;
+
+    const PROMPT: &str = "> ";
+
+    let mut input_state = InputState::Normal;
+    let mut escape_buffer = Vec::new();
+
+    stdout.flush()?;
+    print!("{}", PROMPT);
+    stdout.flush()?;
+
+    loop {
+        let mut buf = [0u8; 1];
+        let _bytes_read = match stdin.read(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => n,
+            Err(e) => {
+                eprintln!("Error reading from stdin: {}", e);
+                break;
+            }
+        };
+        let c = buf[0];
+
+        match input_state {
+            InputState::Normal => match c {
+                b'\x1b' => {
+                    input_state = InputState::Escape;
+                    escape_buffer.clear();
+                }
+                b'q' | b'\x03' => break,
+                b'\n' | b'\r' => {
+                    println!("\r\n{}", line);
+                    line.clear();
+                    cursor_pos = 0;
+                    print!("{}", PROMPT);
+                    stdout.flush()?;
+                }
+                b'\x08' | b'\x7f' => {
+                    if cursor_pos > 0 {
+                        let mut byte_idx_to_remove = 0;
+                        let mut current_char_count = 0;
+                        for (idx, _) in line.char_indices() {
+                            if current_char_count == cursor_pos - 1 {
+                                byte_idx_to_remove = idx;
+                                break;
+                            }
+                            current_char_count += 1;
+                        }
+                        line.remove(byte_idx_to_remove);
+
+                        cursor_pos -= 1;
+
+                        write!(stdout, "\x1b[1D").unwrap();
+                        write!(stdout, "{}\x1b[K", &line[byte_idx_to_remove..]).unwrap();
+                        let chars_after_cursor = line.chars().skip(cursor_pos).count();
+                        if chars_after_cursor > 0 {
+                            write!(stdout, "\x1b[{}D", chars_after_cursor).unwrap();
+                        }
+                        stdout.flush()?;
+                    }
+                }
+                _ => {
+                    if let Some(char_byte) =
+                        str::from_utf8(&[c]).ok().and_then(|s| s.chars().next())
+                    {
+                        if char_byte.is_ascii_graphic()
+                            || (char_byte.is_whitespace() && char_byte != '\t')
+                        {
+                            if cursor_pos == line.chars().count() {
+                                print!("{}", char_byte);
+                                line.push(char_byte);
+                            } else {
+                                let mut byte_idx = 0;
+                                for (idx, _) in line.char_indices().take(cursor_pos) {
+                                    byte_idx = idx;
+                                }
+                                line.insert(byte_idx, char_byte);
+                                write!(stdout, "\x1b[{}D", cursor_pos).unwrap();
+                                write!(stdout, "{}\x1b[K", line).unwrap();
+                                let chars_after_new_cursor =
+                                    line.chars().skip(cursor_pos + 1).count();
+                                if chars_after_new_cursor > 0 {
+                                    write!(stdout, "\x1b[{}D", chars_after_new_cursor).unwrap();
+                                }
+                            }
+                            cursor_pos += 1;
+                            stdout.flush()?;
+                        }
+                    }
+                }
+            },
+            InputState::Escape => {
+                escape_buffer.push(c);
+                if c == b'[' {
+                    input_state = InputState::BracketedEscape;
+                } else {
+                    eprintln!("Unhandled escape sequence: {:?}", escape_buffer);
+                    input_state = InputState::Normal;
+                    escape_buffer.clear();
+                }
+            }
+            InputState::BracketedEscape => {
+                escape_buffer.push(c);
+                match c {
+                    b'A' => {
+                        input_state = InputState::Normal;
+                        escape_buffer.clear();
+                    }
+                    b'B' => {
+                        input_state = InputState::Normal;
+                        escape_buffer.clear();
+                    }
+                    b'C' => {
+                        if cursor_pos < line.chars().count() {
+                            write!(stdout, "\x1b[1C")?;
+                            stdout.flush()?;
+                            cursor_pos += 1;
+                        }
+                        input_state = InputState::Normal;
+                        escape_buffer.clear();
+                    }
+                    b'D' => {
+                        if cursor_pos > 0 {
+                            write!(stdout, "\x1b[1D")?;
+                            stdout.flush()?;
+                            cursor_pos -= 1;
+                        }
+                        input_state = InputState::Normal;
+                        escape_buffer.clear();
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
